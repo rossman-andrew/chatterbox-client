617ad1852f390abbce33be478085e088deaf9fa5
rossman-andrew <andrew100r@gmail.com>
2017-09-02 16:33:47 -0700

    Finished product. Receives and sends messages. Ability to filter messages by roomname. Names of users are displays with messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,9 +5,23 @@ class Application{
 constructor(url, username) {
     //this.username = getUrlVars();
     this.username;
-    this.lastMessagetime = new Date().getTime(); //Date object of last message added
+    this.roomname;
+    this.currentRoomname = "AllChats";
+    this.rooms = [];
+    this.lastMessagetime = new Date().getTime(); //all chats object of last message added
     this.server = url;
     this.getUrlVars();
+    //Prevent XSS Attacks
+    this.entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;',
+        '`': '&#x60;',
+        '=': '&#x3D;'
+    };
 }
 
 init() {
@@ -36,10 +50,23 @@ fetch() {
             for (var msg of results) {
                 //Compare the time of the message,
                 var messageTime = Date.parse(msg.createdAt);
+
+                //Grab the username
                 var username = msg.username;
+
+                //Grab the roomname
+                var roomname = msg.roomname;
+
+                //Iterate over every message
                 if(messageTime > app.lastMessagetime && app.username !== msg.username){
-                    receiveMessage(msg.text, username);
-                    app.lastMessagetime = messageTime; //Date object
+                    receiveMessage(app.escapeHtml(msg.text) , app.escapeHtml(username), app.escapeHtml(roomname)); //Add message to chat block
+                    app.lastMessagetime = messageTime; //Set last date time
+
+                    if(!app.rooms.includes(roomname) && roomname !== undefined && roomname !== null){
+                        app.rooms.push(roomname); //Push to rooms array
+                        addRoomname(roomname); //Add roomname div to drop drop menu
+                    }
+                    
                 }
                 
             }
@@ -89,6 +116,15 @@ clearMessages() {
     $('#chats').empty();
 }
 
+
+
+escapeHtml(string) {
+    var app = this;
+    return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+        return app.entityMap[s];
+    });
+}
+
 unicodeToChar(text) {
    return text.replace(/\\u[\dA-F]{4}/gi, 
           function (match) {
@@ -114,16 +150,17 @@ console.log(app);
 (function () {
     var Message;
     Message = function (arg) {
-        this.text = arg.text, this.message_side = arg.message_side, this.username = arg.username;
+        this.text = arg.text, this.message_side = arg.message_side, this.username = arg.username, this.roomname = arg.roomname;
         this.draw = function (_this) {
             return function () {
                 var $message;
                 $message = $($('.message_template').clone().html());
                 $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $message.addClass(this.roomname)
                 console.log(this.username);
                 $message.find('.username').html(this.username);
                 $('.messages').append($message);
-                
+
                 return setTimeout(function () {
                     return $message.addClass('appeared');
                 }, 0);
@@ -134,12 +171,61 @@ console.log(app);
     $(function () {
         var getMessageText, message_side, sendMessage;
         message_side = 'right';
+
+        //Create All Chats button
+        var $allChats = $("<a>", {class: "dropdown-item dropdown_template allChats", href: "#"});
+            $allChats.text('All Chats');
+            $allChats.click(function(){ 
+                $('.message').show();
+                $('.title').text('All Chats');});
+            $('.dropdown-menu').append($allChats);
+
         getMessageText = function () {
             var $message_input;
             $message_input = $('.message_input');
             return $message_input.val();
         };
-        receiveMessage = function(text, username) {
+
+        //Add new roomname
+        addRoomname = function (roomname) {
+            // $(".dropdown-menu").append("<a></a>")
+            var $roomname;
+            var $tag = $("<a>", {class: "dropdown-item dropdown_template"});
+            $tag.text(roomname);
+            
+            //Add click function to new roomname in dropdown menu
+            $tag.click(function(){ 
+                console.log(roomname);
+                
+                //Change the title of the chat window to current chat room
+                $('.title').text(roomname);
+
+                //Set Current room to the roomname
+                app.currentRoomname = roomname;
+
+                //Hide all messages that are not apart of the current room
+                filterRooms(roomname);
+            });
+
+            $('.dropdown-menu').append($tag);
+        };
+
+        filterRooms = function (roomname) {
+            $("ul.messages").children("li.message").each(function () {
+                console.log()
+
+                if (!($(this).hasClass(roomname))) {
+                    console.log('This element contains the room name and should be hidden');
+                    $(this).hide();
+                }else{
+                    console.log('The element does not contain the class name ', roomname);
+                }
+            });
+                //$("ul.messages").children().hasClass(roomname);
+            
+        }
+
+        receiveMessage = function (text, username, roomname) {
             console.log('receive was called');
             var $messages, message;
             if (text.trim() === '') {
@@ -151,14 +237,16 @@ console.log(app);
             message = new Message({
                 text: text,
                 message_side: message_side,
-                username: username
-            }); 
+                username: username,
+                roomname: roomname
+
+            });
             message.draw();
 
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
-        sendMessage = function (text) {
-            
+        sendMessage = function (text, roomname) {
+
             var $messages, message;
             if (text.trim() === '') {
                 return;
@@ -169,7 +257,8 @@ console.log(app);
             message = new Message({
                 text: text,
                 message_side: message_side,
-                username: app.username
+                username: app.username,
+                roomname: roomname
             });
             message.draw();
 
@@ -183,19 +272,19 @@ console.log(app);
 
             return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
         };
-        
+
         //Call the fetch function every 5 seconds
-        setInterval(function() {
+        setInterval(function () {
             app.fetch();
         }, 5000);
 
 
         $('.send_message').click(function (e) {
-            return sendMessage(getMessageText());
+            return sendMessage(getMessageText(), app.currentRoomname);
         });
         $('.message_input').keyup(function (e) {
             if (e.which === 13) {
-                return sendMessage(getMessageText());
+                return sendMessage(getMessageText(), app.currentRoomname);
             }
         });
     });
